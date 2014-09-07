35adf292c946c3e6d0c32bd86960ce2b7c77d5b2
Stefan Zeller <stefan.zeller@docksnet.ch>
2014-09-07 13:24:59 +0200

    Talks können kommentiert werden. Vorerst sind Kommentare statisch und unabhängig von talkId. Neu liefert ein UserService den aktuell eingeloggten User (auch gemockt)

--- app/scripts/controllers/TrackController.js
+++ app/scripts/controllers/TrackController.js
@@ -16,26 +16,20 @@ angular.module('conferenceBuddyApp').controller('TrackController',
         dialogService.showError('Backend Error', 'Failed to load conference data from the backend', err.data + ' HTTP-Status:' + err.status);
     });
 
-    $scope.formatSpeakers = function(talk) {
-        var speakers = '';
-        talk.speakers.forEach(function(speaker) {
-            if (speakers !== '') {
-                speakers += ' & ';
-            }
-            speakers += speaker.name + ' ' + speaker.surname;
-        });
-        return speakers;
-    };
-    $scope.nextTrack = function() {
-        if ($scope.hasNextTrack()) {
-            currentTrackIndex += 1;
-            updateTrack();
-        }
-    };
+		commentService.load().then(function (data) {
+			$scope.comments = data;
+		});
 
-    $scope.hasNextTrack = function() {
-        return $scope.conference.tracks.length > currentTrackIndex + 1;
-    };
+		$scope.formatSpeakers = function (talk) {
+			var speakers = '';
+			talk.speakers.forEach(function (speaker) {
+				if (speakers !== '') {
+					speakers += ' & ';
+				}
+				speakers += speaker.name + ' ' + speaker.surname;
+			});
+			return speakers;
+		};
 
     $scope.previousTrack = function() {
         if ($scope.hasPreviousTrack()) {
@@ -63,9 +57,16 @@ angular.module('conferenceBuddyApp').controller('TrackController',
         return talk.abstract && talk.abstract.length > 0;
     };
 
-    $scope.showMyTrack = function() {
-        $location.path('mytrack');
-    };
+		$scope.showDetails = function (index) {
+			var talk = $scope.currentTrack.talks[index];
+			if ($scope.hasAbstract(talk)) {
+				var options = {
+					talk: talk,
+					formatSpeakers: $scope.formatSpeakers
+				};
+				dialogService.showModal({templateUrl: 'partials/talk.html'}, options);
+			}
+		};
 
     function updateTrack() {
         $scope.currentTrack = $scope.conference.tracks[currentTrackIndex];
@@ -76,9 +77,4 @@ angular.module('conferenceBuddyApp').controller('TrackController',
         $scope.copperfield = $scope.copperfield ? '' : 'magic';
     }
 
-	}])
-	.directive('comments', function () {
-		return {
-			templateUrl: 'templates/comments-template.html'
-		};
-	});
+	}]);
--- app/scripts/services/CommentService.js
+++ app/scripts/services/CommentService.js
@@ -4,11 +4,15 @@ angular.module('conferenceBuddyApp')
 	.factory('CommentService', ['$http', function ($http) {
 
 		return  {
-			load: function () {
+			load: function (talkId) {
+				console.log(talkId);
 				return $http.get('api/comments.json')
 					.then(function (result) {
 						return result.data;
 					});
+			},
+			save: function(commentEntry) {
+				console.log('Save comment [' + commentEntry.author + ', ' + commentEntry.comment + ']');
 			}
 		};
 
