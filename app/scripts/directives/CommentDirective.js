886a0ad53d48c518fc629398151a1018bcf80998
mwalter <mwalter@newinstance.org>
2014-09-08 21:10:16 +0200

    added date functionality to comments (broken)

--- app/scripts/directives/CommentDirective.js
+++ app/scripts/directives/CommentDirective.js
@@ -13,12 +13,16 @@ angular.module('conferenceBuddyApp').directive('comments', ['CommentService', 'U
 
             commentService.load($scope.talkId).then(function(data) {
                 $scope.comments = data;
+                var i = $scope.comments[0];
+                console.log(i.time);
             });
 
             $scope.submitForm = function(isValid) {
                 if (isValid) {
                     var commentEntry = angular.copy($scope.commentEntry);
                     commentEntry.author = $scope.author;
+                    commentEntry.time = JSON.stringify(new Date());
+                    console.log(commentEntry.time);
                     commentService.save(commentEntry);
                     $scope.comments.splice(0, 0, commentEntry);
                     $scope.commentEntry = null;
--- app/scripts/services/CommentService.js
+++ app/scripts/services/CommentService.js
@@ -1,19 +1,17 @@
 'use strict';
 
-angular.module('conferenceBuddyApp')
-	.factory('CommentService', ['$http', function ($http) {
+angular.module('conferenceBuddyApp').factory('CommentService', ['$http', function($http) {
 
-		return  {
-			load: function (talkId) {
-				console.log(talkId);
-				return $http.get('api/comments.json')
-					.then(function (result) {
-						return result.data;
-					});
-			},
-			save: function(commentEntry) {
-				console.log('Save comment [' + commentEntry.author + ', ' + commentEntry.comment + ']');
-			}
-		};
+    return {
+        load: function(talkId) {
+            console.log(talkId);
+            return $http.get('api/comments.json').then(function(result) {
+                return result.data;
+            });
+        },
+        save: function(commentEntry) {
+            console.log('Save comment [' + commentEntry.author + ', ' + commentEntry.comment + ', ' + commentEntry.time + ']');
+        }
+    };
 
-	}]);
+}]);
