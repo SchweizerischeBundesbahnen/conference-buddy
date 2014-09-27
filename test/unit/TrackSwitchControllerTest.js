c5c7aa18330a8d067b05c26ef55ac4b05c65714e
Stefan Zeller <stefan.zeller@docksnet.ch>
2014-09-27 14:32:07 +0200

    cleanup test

--- test/unit/TrackSwitchControllerTest.js
+++ test/unit/TrackSwitchControllerTest.js
@@ -2,7 +2,7 @@
 
 describe('Controller: TrackSwitchController', function() {
 
-    var scope, createController, rootScope, location, dialogService, myTrackService, commentService;
+    var scope, createController, rootScope, location;
 
     beforeEach(function() {
 
@@ -10,56 +10,7 @@ describe('Controller: TrackSwitchController', function() {
             // configure $provide constants etc..
         });
 
-        angular.mock.inject(function($rootScope, $q, $controller, $location, ConferenceService, DialogService, MyTrackService, CommentService) {
-
-            var mockedConf = {tracks: [
-                {id: 'teaTime', title: 'The Art of Tea',
-                    presentations: [
-                        {speakers: [
-                            {name: 'Mr.', surname: 'Pink'},
-                            {name: 'Mr.', surname: 'White'}
-                        ],
-                            abstract: 'hello'},
-                        {speakers: [
-                            {name: 'Mr.', surname: 'White'}
-                        ]}
-                    ]},
-                {id: 'coffeeTime', title: 'A Rush of Caffeine to the Head'}
-            ]};
-
-            // mock 'load' resolving the promise
-            var deferred = $q.defer();
-            deferred.resolve(mockedConf);
-            spyOn(ConferenceService, 'load').andReturn(deferred.promise);
-
-            var mockedMyTrack = ['a1', 'a2', 'a3'];
-
-            // mock 'load' resolving the promise
-            deferred = $q.defer();
-            deferred.resolve(mockedMyTrack);
-            spyOn(MyTrackService, 'load').andReturn(deferred.promise);
-
-            var mockedComments = [
-                {
-                    'author': {
-                        'name': 'Ellie McInelli'
-                    },
-                    'comment': 'Supergeil!',
-                    'timestamp': '2014-09-08T18:25:00.000Z'
-                },
-                {
-                    'author': {
-                        'name': 'Axel Schweiss'
-                    },
-                    'comment': 'Worum gehts hier?',
-                    'timestamp': '2014-09-08T18:26:00.000Z'
-                }
-            ];
-
-            // mock 'load' resolving the promise
-            deferred = $q.defer();
-            deferred.resolve(mockedComments);
-            spyOn(CommentService, 'load').andReturn(deferred.promise);
+        angular.mock.inject(function($rootScope, $q, $controller, $location) {
 
             rootScope = $rootScope;
 
@@ -72,10 +23,6 @@ describe('Controller: TrackSwitchController', function() {
             createController = function() {
                 return $controller('TrackSwitchController', {$scope: scope});
             };
-
-            dialogService = DialogService;
-            myTrackService = MyTrackService;
-            commentService = CommentService;
 
         });
     });
