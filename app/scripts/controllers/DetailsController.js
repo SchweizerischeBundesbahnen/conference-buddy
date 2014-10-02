a40532c1296f66312338fe607fc3269861e0c39a
Manuel Friedli <manuel@fritteli.ch>
2014-10-03 01:18:14 +0200

    nex version of rating. the API has changed, now the server should calculate the average rating and also determine whether the user has already rated a presentation or not.

--- app/scripts/controllers/DetailsController.js
+++ app/scripts/controllers/DetailsController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conferenceBuddyApp').controller('DetailsController',
-    ['$scope', '$routeParams', '$window', 'ConferenceService', function($scope, $routeParams, $window, conferenceService) {
+    ['$scope', '$routeParams', '$window', 'ConferenceService', 'RatingService', function($scope, $routeParams, $window, conferenceService, ratingService) {
 
     $scope.conference = {tracks: [ ]};
     $scope.presentation = null;
@@ -9,10 +9,31 @@ angular.module('conferenceBuddyApp').controller('DetailsController',
     conferenceService.load().then(function(conference) {
         $scope.conference = conference;
         $scope.presentation = conferenceService.currentPresentation();
+        ratingService.load($scope.presentation.id).then(function(rating) {
+            $scope.hasRatings = (rating.average !== undefined);
+            if ($scope.hasRatings) {
+                $scope.averageRating = rating.average;
+            }
+            $scope.hasMyRating = (rating.myRating !== undefined);
+            $scope.myRating = $scope.hasMyRating ? rating.myRating : 0;
+        });
     });
 
     $scope.goBack = function() {
         $window.history.back();
-    }
+    };
 
+    $scope.rate = function() {
+        var newRating;
+
+        if ($scope.hasMyRating) {
+            newRating = ratingService.update($scope.presentation.id, $scope.myRating);
+        } else {
+            newRating = ratingService.save($scope.presentation.id, $scope.myRating);
+            $scope.hasMyRating = true;
+        }
+
+        $scope.hasRatings = true;
+        $scope.averageRating = newRating.average;
+    };
 }]);
