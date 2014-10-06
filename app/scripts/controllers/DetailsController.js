b495daa4f1676c4997d1b79139b8d1e39716852e
Manuel Friedli <manuel@fritteli.ch>
2014-10-06 11:57:24 +0200

    when on details view, redirect to home page if no current presentation ist loaded

--- app/scripts/controllers/DetailsController.js
+++ app/scripts/controllers/DetailsController.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('conferenceBuddyApp').controller('DetailsController',
-    ['$scope', '$routeParams', '$window', 'ConferenceService', 'RatingService', function($scope, $routeParams, $window, conferenceService, ratingService) {
+    ['$scope', '$routeParams', '$window', '$location', 'ConferenceService', 'RatingService', function($scope, $routeParams, $window, $location,
+        conferenceService, ratingService) {
 
     $scope.conference = {tracks: [ ]};
     $scope.presentation = null;
@@ -9,14 +10,21 @@ angular.module('conferenceBuddyApp').controller('DetailsController',
     conferenceService.load().then(function(conference) {
         $scope.conference = conference;
         $scope.presentation = conferenceService.currentPresentation();
-        ratingService.load($scope.presentation.id).then(function(rating) {
-            $scope.hasRatings = (rating.average !== undefined);
-            if ($scope.hasRatings) {
-                $scope.averageRating = rating.average;
-            }
-            $scope.hasMyRating = (rating.myRating !== undefined);
-            $scope.myRating = $scope.hasMyRating ? rating.myRating : 0;
-        });
+        if ($scope.presentation == null) {
+            // presentation is null or undefined. can happen if the user has the details page open and closes the browser. when he re-opens the
+            // browser and restores the open tabs from his last session, there will be no current presentation. in that case, we will redirect him
+            // to the home page
+            $location.url('/');
+        } else {
+            ratingService.load($scope.presentation.id).then(function(rating) {
+                $scope.hasRatings = (rating.average !== undefined);
+                if ($scope.hasRatings) {
+                    $scope.averageRating = rating.average;
+                }
+                $scope.hasMyRating = (rating.myRating !== undefined);
+                $scope.myRating = $scope.hasMyRating ? rating.myRating : 0;
+            });
+        }
     });
 
     $scope.goBack = function() {
