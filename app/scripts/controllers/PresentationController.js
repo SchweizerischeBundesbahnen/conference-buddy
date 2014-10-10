b1629b21c605f4ecbed892b3f460e387816b9881
kguelseven <kguelseven@gmail.com>
2014-10-11 00:11:50 +0200

    fixed top navbar, route paths as constant ROUTES

--- app/scripts/controllers/PresentationController.js
+++ app/scripts/controllers/PresentationController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('conferenceBuddyApp').controller('PresentationController', ['$scope', '$location', 'ConferenceService', function($scope, $location, conferenceService) {
+angular.module('conferenceBuddyApp').controller('PresentationController', ['$scope', '$location', 'ConferenceService', 'ROUTES', function($scope, $location, conferenceService, ROUTES) {
 
     $scope.formatSpeakers = function(presentation) {
         var speakers = '';
@@ -17,11 +17,11 @@ angular.module('conferenceBuddyApp').controller('PresentationController', ['$sco
         return presentation.abstract && presentation.abstract.length > 0;
     };
 
-    $scope.showDetails = function(index, track) {
+    $scope.showDetails = function(index, track, contextUrl) {
         var presentation = track.presentations[index];
         if ($scope.hasAbstract(presentation)) {
             conferenceService.selectPresentation(presentation);
-            $location.path('/details');
+            $location.path(contextUrl + ROUTES.DETAILS);
         }
     };
 
