4786e4f54bf3e4b37162a3f45c8ea59b0a732cfc
Stefan Zeller <stefan.zeller@docksnet.ch>
2014-11-02 13:24:49 +0100

    simple reload button for comments. Also show random comment on reload.

--- app/scripts/services/CommentService.js
+++ app/scripts/services/CommentService.js
@@ -2,9 +2,23 @@
 
 angular.module('conferenceBuddyApp').factory('CommentService', ['$http', function($http) {
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
     return {
         load: function(talkId) {
             return $http.get('api/comments.json').then(function(result) {
+                var number = getRandomInt(0, 100);
+                var text = 'Random Text: ' + number;
+
+                result.data.push({
+                    'author': {
+                        'name': 'Random User'
+                    },
+                    'comment': text,
+                    'timestamp': '2014-09-08T18:26:00.000Z'
+                });
                 return result.data;
             });
         },
