2d9343d0585feadcebbacf532d62350ce52c0400
kguelseven <kguelseven@gmail.com>
2014-11-11 22:52:31 +0100

    removed console.log

--- app/scripts/services/RatingService.js
+++ app/scripts/services/RatingService.js
@@ -7,12 +7,10 @@ angular.module('conferenceBuddyApp').factory('RatingService', ['$http', 'UserSer
     return {
         load: function(presentationId) {
             return $http.get(REST_URL + '/rating/' + presentationId).then(function(result) {
-                console.log('rating', result);
                 return result.data;
             });
         },
         save: function(presentationId, rating) {
-            console.log('rating-save', rating);
             return $http.put(REST_URL + '/rating/' + presentationId, rating).then(function(result) {
                 return result.data;
             });
