11dfe6740c5a9d8febeaef3b41ef8d8aab72bbbc
tuabache <tuabache@gmail.com>
2015-04-03 00:09:59 +0200

    Merge branch 'develop' of https://github.com/SchweizerischeBundesbahnen/conference-buddy into feature/172_clientbuild
    Conflicts:
    	Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,8 +129,6 @@ module.exports = function(grunt) {
             }
         },
 
-
-
         // Make sure code styles are up to par and there are no obvious mistakes
         jshint: {
             options: {
